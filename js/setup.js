87fe0f5c682cc81e86f87a14b9bbde4698bf8e96
usbt0p <lucascdeaa@gmail.com>
2025-10-07 15:12:44 +0200

    dynamic title + link colour

--- js/setup.js
+++ js/setup.js
@@ -22,3 +22,23 @@ fetch('footer.html')
     .catch(error => {
         console.error('Error loading footer:', error);
     });
+
+function setDynamicPageTitle() {
+    // Get the current file name from the URL
+    let fileName = window.location.pathname.split('/').pop().split('.html')[0];
+    
+    if (fileName == "index") {
+        fileName = "home";
+    }
+    
+    if (!fileName) {
+        fileName = 'page';
+    }
+
+    // Capitalize the first letter and set the title
+    const pageTitle = fileName.charAt(0).toUpperCase() + fileName.slice(1);
+    document.title = `${pageTitle} - usbt0p`;
+}
+
+// Call the function to set the page title
+setDynamicPageTitle();
